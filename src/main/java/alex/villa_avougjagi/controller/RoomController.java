f3c4e91e25ac5e7fad874e07086e6e68722fe287
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 17:11:38 +0200

    Lagt till visning av rum

--- src/main/java/alex/villa_avougjagi/controller/RoomController.java
+++ src/main/java/alex/villa_avougjagi/controller/RoomController.java
@@ -1,4 +1,21 @@
 package alex.villa_avougjagi.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rooms")
+@RequiredArgsConstructor
 public class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping
+    public String listRooms(Model model) {
+        model.addAttribute("rooms", roomService.findAll());
+        return "customers/room/list";
+    }
 }
