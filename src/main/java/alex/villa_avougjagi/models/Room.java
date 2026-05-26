6269aef294afc09a32c945dc21e3d6f6fc7911d3
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:07:52 +0200

    Fixat room-klassen med extrasängar och pris

--- src/main/java/alex/villa_avougjagi/models/Room.java
+++ src/main/java/alex/villa_avougjagi/models/Room.java
@@ -1,4 +1,41 @@
 package alex.villa_avougjagi.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String roomNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoomType roomType;
+
+    @Min(0)
+    @Max(2)
+    private int extraBeds;
+
+    @Min(0)
+    @Builder.Default
+    private double pricePerNight = 995;
+
+    public int getCapacity() {
+        return roomType.getBaseCapacity() + extraBeds;
+    }
 }
