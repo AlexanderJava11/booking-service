2d69040b1e91f9ac228506be6641be27db064fa1
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:58:44 +0200

    Lagt till möjlighet att ändra kunder

--- src/main/java/alex/villa_avougjagi/controller/CustomerController.java
+++ src/main/java/alex/villa_avougjagi/controller/CustomerController.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
@@ -40,6 +37,18 @@ public class CustomerController {
         }
         customerService.save(customerDTO);
         return "redirect:/customers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        CustomerDTO customer = customerService.findById(id);
+
+        if (customer == null) {
+            return "redirect:/customers";
+        }
+
+        model.addAttribute("customer", customer);
+        return "customers/form";
     }
 
 }
