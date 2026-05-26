f3a2a6368e227aef697fb5a7f062d3e2ac2168a4
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 15:59:29 +0200

    Lagt till bokningar och kopplingar till rum

--- src/main/java/alex/villa_avougjagi/models/Booking.java
+++ src/main/java/alex/villa_avougjagi/models/Booking.java
@@ -1,4 +1,34 @@
 package alex.villa_avougjagi.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Customer customer;
+
+    @ManyToOne(optional = false)
+    private Room room;
+
+    @NotNull
+    private LocalDate checkInDate;
+
+    @NotNull
+    private LocalDate checkOutDate;
+
+    @Min(1)
+    private int numberOfGuests;
 }
