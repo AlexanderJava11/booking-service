6b7e2bbff01224f68c8ee9da0f819aad4280fa76
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 17:16:04 +0200

    Fixat validering och sparning för rum

--- src/main/java/alex/villa_avougjagi/controller/RoomController.java
+++ src/main/java/alex/villa_avougjagi/controller/RoomController.java
@@ -1,9 +1,14 @@
 package alex.villa_avougjagi.controller;
 
+import alex.villa_avougjagi.models.RoomType;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +22,26 @@ public class RoomController {
     public String listRooms(Model model) {
         model.addAttribute("rooms", roomService.findAll());
         return "customers/room/list";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("room", new RoomDTO());
+        model.addAttribute("roomTypes", RoomType.values());
+        return "customers/room/form";
+    }
+
+    @PostMapping("/save")
+    public String saveRoom(@Valid @ModelAttribute("room") RoomDTO roomDTO,
+                           BindingResult result,
+                           Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("roomTypes", RoomType.values());
+            return "customers/room/form";
+        }
+
+        roomService.save(roomDTO);
+        return "redirect:/rooms";
     }
 }
