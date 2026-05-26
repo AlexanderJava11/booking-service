f833c341607d687c4dba5a333663b76f042f936d
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:12:36 +0200

    Fixat rumstyper för enkelrum och dubbelrum

--- src/main/java/alex/villa_avougjagi/models/RoomType.java
+++ src/main/java/alex/villa_avougjagi/models/RoomType.java
@@ -1,4 +1,23 @@
 package alex.villa_avougjagi.models;
 
-public class RoomType {
+public enum RoomType {
+
+    SINGLE ("Enkelrum", 1),
+    DOUBLE ("Dubbelrum", 2);
+
+    private final String displayName;
+    private final int baseCapacity;
+
+    RoomType(String displayName, int baseCapacity) {
+        this.displayName = displayName;
+        this.baseCapacity = baseCapacity;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getBaseCapacity() {
+        return baseCapacity;
+    }
 }
