f14d5970542554a0d661e91af3b2fb9ecba8505b
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:02:31 +0200

    Lagt till customer model och validering

--- src/main/java/alex/villa_avougjagi/models/Customer.java
+++ src/main/java/alex/villa_avougjagi/models/Customer.java
@@ -1,4 +1,42 @@
 package alex.villa_avougjagi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]{1,4}?[ .-]?[0-9]{6,12}$" )
+    private String phone;
 }
