af8d00b9bb68e70ab8c4adba786645c706a8292b
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:51:02 +0200

    Fixat kundlistan

--- src/main/java/alex/villa_avougjagi/controller/CustomerController.java
+++ src/main/java/alex/villa_avougjagi/controller/CustomerController.java
@@ -1,4 +1,25 @@
 package alex.villa_avougjagi.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    @GetMapping
+    public String listCustomers(Model model) {
+        model.addAttribute("customers", customerService.findAll());
+        return "customers/list";
+    }
+
 }
