8ed6a5e7314dea2ad23ae584cc5f957bb240f112
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 17:18:47 +0200

    Lagt till möjlighet att ändra rum

--- src/main/java/alex/villa_avougjagi/controller/RoomController.java
+++ src/main/java/alex/villa_avougjagi/controller/RoomController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/rooms")
@@ -43,5 +40,12 @@ public class RoomController {
 
         roomService.save(roomDTO);
         return "redirect:/rooms";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        model.addAttribute("room", roomService.findById(id));
+        model.addAttribute("roomTypes", RoomType.values());
+        return "customers/room/form";
     }
 }
