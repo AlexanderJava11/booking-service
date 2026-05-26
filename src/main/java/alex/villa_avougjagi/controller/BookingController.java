17d8d5175ee8cb4eb6fc49540b4472dc0d52efb3
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 16:20:13 +0200

    Fixat sida för att visa alla bokningar

--- src/main/java/alex/villa_avougjagi/controller/BookingController.java
+++ src/main/java/alex/villa_avougjagi/controller/BookingController.java
@@ -1,4 +1,23 @@
 package alex.villa_avougjagi.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
 public class BookingController {
+
+    private final CustomerService customerService;
+    private final RoomService roomService;
+    private final BookingService bookingService;
+
+    @GetMapping
+    public String listBookings(Model model) {
+        model.addAttribute("bookings", bookingService.findAll());
+        return "customers/bookings/list";
+    }
 }
