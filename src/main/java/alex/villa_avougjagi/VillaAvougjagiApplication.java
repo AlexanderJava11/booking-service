571c3a676507a4fa55ba006990c681e4cc9a951a
Aleks <alexander.zeljic@yh.nackademin.se>
2026-05-26 14:32:08 +0200

    First start. Added folders and test that spring boot working.

---
+++ src/main/java/alex/villa_avougjagi/VillaAvougjagiApplication.java
@@ -0,0 +1,13 @@
+package alex.villa_avougjagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaAvougjagiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillaAvougjagiApplication.class, args);
+    }
+
+}
