50abf6bb315b4d3256c3a2f3ca7a3dd60796c940
Aleks <alexander.zeljic@yh.nackademin.se>
2026-09-21 21:01:11 +0200

    Remove customer persistence from booking service

--- src/main/java/alex/villa_avougjagi/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package alex.villa_avougjagi.repositories;
-
-import alex.villa_avougjagi.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
